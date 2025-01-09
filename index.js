e07a462bc73432648411b068a98b490b71654106
Abdul Hadi <ilmnoor278@gmail.com>
2025-01-09 11:56:01 +0500

    added domain into cors

--- index.js
+++ index.js
@@ -11,7 +11,9 @@ import commentRouter from "./src/routers/comment.router.js";
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173'
+}));
 app.use(cookieParser());
 
 app.use("/api/v1", userRouter);
